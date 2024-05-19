e2e82cbb884a4566583c66e6e38cc7a9929acfda
Youssef El Idrissi <professionalidrissi@gmail.com>
2024-05-19 23:59:04 +0100

    added: amenity model

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" this file contains base model """
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ This class is for Amenity """
+    name = ""
